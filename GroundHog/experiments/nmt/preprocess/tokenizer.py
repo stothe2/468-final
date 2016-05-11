c2b08ce0241f59ac7563bb151eecb97761a87aec
Sachi Sanghavi <sachi@jhu.edu>
2016-05-11 07:05:12 -0400

    Add script

---
+++ GroundHog/experiments/nmt/preprocess/tokenizer.py
@@ -0,0 +1,29 @@
+import optparse
+import sys, os
+import codecs
+from nltk.tokenize import TweetTokenizer
+
+baseline_path = os.getcwd()[:-36]
+if baseline_path not in sys.path:
+    sys.path.insert(0, baseline_path)
+
+optparser = optparse.OptionParser()
+optparser.add_option("-d", "--data", dest="data", default=baseline_path+"en-jp-gold/bitext/parallel.gold.en-jp", help="Parallel corpus")
+(opts, _) = optparser.parse_args()
+
+ref = [pair.split(' ||| ') for pair in open(opts.data)]
+num_sents = len(ref)
+
+tknzr = TweetTokenizer()
+
+en_tokenized = codecs.open("bitext.en.tok.txt", "w", "utf-8")
+ja_tokenized = codecs.open("bitext.ja.tok.txt", "w", "utf-8")
+
+for pair in ref:
+    en_tokenized.write(" ".join(tknzr.tokenize(pair[0])))
+    en_tokenized.write("\n")
+    ja_tokenized.write(" ".join(tknzr.tokenize(pair[1].decode("utf-8"))))
+    ja_tokenized.write("\n")
+
+en_tokenized.close()
+ja_tokenized.close()
