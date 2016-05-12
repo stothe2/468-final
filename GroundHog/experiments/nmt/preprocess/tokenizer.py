0fd6ea03e4efee25298c5787b489d7c994caaf4a
Sarah Watanabe <swatana3@jhu.edu>
2016-05-11 22:42:03 -0400

    modified tokenizer.py to tokenize Japanese better

--- GroundHog/experiments/nmt/preprocess/tokenizer.py
+++ GroundHog/experiments/nmt/preprocess/tokenizer.py
@@ -2,6 +2,12 @@ import optparse
 import sys, os
 import codecs
 from nltk.tokenize import TweetTokenizer
+import nltk.tokenize.api
+import tinysegmenter
+
+class myTinySegmenter(tinysegmenter.TinySegmenter, nltk.tokenize.api.TokenizerI):
+    pass
+
 
 baseline_path = os.getcwd()[:-36]
 if baseline_path not in sys.path:
@@ -15,6 +21,7 @@ ref = [pair.split(' ||| ') for pair in open(opts.data)]
 num_sents = len(ref)
 
 tknzr = TweetTokenizer()
+segmenter = myTinySegmenter()
 
 en_tokenized = codecs.open("bitext.en.tok.txt", "w", "utf-8")
 ja_tokenized = codecs.open("bitext.ja.tok.txt", "w", "utf-8")
@@ -22,7 +29,8 @@ ja_tokenized = codecs.open("bitext.ja.tok.txt", "w", "utf-8")
 for pair in ref:
     en_tokenized.write(" ".join(tknzr.tokenize(pair[0])))
     en_tokenized.write("\n")
-    ja_tokenized.write(" ".join(tknzr.tokenize(pair[1].decode("utf-8"))))
+    print("ref is " + pair[1])
+    ja_tokenized.write(" ".join(segmenter.tokenize(pair[1].decode("utf-8"))))
     ja_tokenized.write("\n")
 
 en_tokenized.close()
